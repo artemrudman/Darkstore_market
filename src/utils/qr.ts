84a84fa469d963bbf1443a9633b13ae8a32db852
xysed <xysed2969@yandex.ru>
2023-08-27 16:12:45 +0000

    TODO

--- src/utils/qr.ts
+++ src/utils/qr.ts
@@ -1,6 +1,8 @@
 import { SHA256 } from "crypto-js";
 import { Pool } from "pg";
 
+// TODO: Генерировать строку из рандомных символов
+
 function generateHex() {
     const hexRef = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
     let result = [];
