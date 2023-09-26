bdb1364646cbe92bd4593af7b3bf2705a5ec928a
xysed <xysed2969@yandex.ru>
2023-09-26 22:57:30 +0700

    Upd

--- tables_v2.sql
+++ tables_v2.sql
@@ -44,19 +44,26 @@ CREATE TABLE branch(
   ]
   */
   qr CHAR(64) NOT NULL,
-  log_id INTEGER NOT NULL
 );
 
 /* TODO: create table branch_schedule_extra */
 
-CREATE TABLE branch_items(
+CREATE TABLE branch_item(
   id SERIAL PRIMARY KEY,
   branch_id INTEGER NOT NULL,
   item_id INTEGER NOT NULL,
   shelf_amount JSONB NOT NULL,
-  expires_date TIMESTAMP NOT NULL,
+  expires_at TIMESTAMP NOT NULL,
   is_sale BOOLEAN NOT NULL,
   price FLOAT NOT NULL,
   sale_price FLOAT NOT NULL,
-  log_id INTEGER NOT NULL
+);
+
+CREATE TABLE branch_shelf(
+  id SERIAL PRIMARY KEY,
+  branch_id INTEGER NOT NULL,
+  name VARCHAR(5) NOT NULL,
+  storage_type_id INTEGER NOT NULL,
+  is_disabled BOOLEAN NOT NULL,
+  qr CHAR(64) NOT NULL,
 );
